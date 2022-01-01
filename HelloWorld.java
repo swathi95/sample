f1023a2a87eb1057aedc5da3390e98ca11b49a89
swathi95 <swathi2195@gmail.com>
2022-01-01 18:15:31 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,6 @@ int a= 0,b=0;
 System.out.pintln(a);
 
 int c = a +b; /// i don't want this code in main branch,so create a new branch & add this 
-System.out.prinltn(c);
+System.out.prinltn( "the sum of two variables is" +c); // now both the repository will have 2 updated files & we are going to merge these files
 }
 }
