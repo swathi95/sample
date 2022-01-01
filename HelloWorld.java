50535a1f9f3e71fc5a5ce83beb88f86cb01fcf1e
swathi95 <swathi2195@gmail.com>
2022-01-01 18:09:03 +0530

    Helloworld & addition of two numbers

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,10 @@ Class Abc
 {
 public static void main(String args[]){
 System.out.println("hello");
-int a= 0;
+int a= 0,b=0;
 System.out.pintln(a);
+
+int c = a +b; /// i don't want this code in main branch,so create a new branch & add this 
+System.out.prinltn(c);
 }
 }
